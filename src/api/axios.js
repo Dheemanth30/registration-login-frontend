9d4a430c7d0fdbd9e88a52b854170513eec7c9f4
Dheemanth <dheemanthnk15@gmail.com>
2026-09-20 18:07:17 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/auth',
+ baseURL: 'https://registration-login-backend-2fnb.onrender.com/api/auth'
   withCredentials: true,
 });
 
